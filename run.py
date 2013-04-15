bf8ac6effe7906f0a9bb1e123fd3843c302ac050
convict <convict@jailcell.org>
2013-04-15 15:48:56 -0400

    Now updates all active groups.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import utils.db as db
 import utils.helper as helper
+import sys
 from pprint import pprint
 
 NNTP_INFO = helper.getUsenetInfo()
@@ -9,28 +10,39 @@ socket = helper.con(*NNTP_INFO)
 # get the welcome msg
 print socket.getwelcome()
 
-# change group and get info
-_, _, first, last, _ = socket.group('alt.binaries.x264')
+# get list of active groups
+activeGroups = db.getActiveGroups()
+if len(activeGroups) == 0:
+	print 'There are no groups activated. Exiting.'
+	sys.exit(-1)
 
-# get groupID and last local article
-gid = db.getGroupID('alt.binaries.x264')
-lastDBArticle = db.getLastArticle(gid)
+for i in activeGroups:
+	gid = i
+	groupName = db.getGroupName(gid)
 
-# get last 100 articles on server if we don't ahve any
-if lastDBArticle == 0:
-	lastDBArticle = last - 100
+	# change group and get info
+	_, _, first, last, _ = socket.group(groupName)
+	
+	# get last local article
+	lastDBArticle = db.getLastArticle(gid)			
 
-# get article headers and add them to the DB
-resp, overviews = socket.over((lastDBArticle, last))
-db.addParts(gid, overviews)
+	# get last 100 articles on server if we don't ahve any	
+	if lastDBArticle == 0:
+		lastDBArticle = last - 100
+	
+	# get article headers and add them to the DB
+	resp, overviews = socket.over((lastDBArticle, last))
+	db.addParts(gid, overviews)
+	
+	# update the group with new info
+	db.updateGroup(gid, last)
+	
+	# stats
+	print 'Updated group %s' % groupName
+	print 'Last local article num: %s' % lastDBArticle
+	print 'Last server article num: %s' % last
+	print "Added %d parts.\n\n" % (last-lastDBArticle)
 
-# update the group with new info
-db.updateGroup(gid, last)
 
 # CLOSE THE SOCKET FOR THE LOVE OF GOD CLOSE THE FUCKING SOCKET
 socket.quit()
-
-# stats
-print 'Last local article num: %s' % lastDBArticle
-print 'Last server article num: %s' % last
-print 'Added %d parts.' % (last-lastDBArticle)
