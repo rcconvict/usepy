7214b9b4c2cbbc6da271563b3e6376b3891072d5
convict <convict@jailcell.org>
2013-04-15 14:19:25 -0400

    Added getGroupID() and fixed getLastArticle().

--- utils/db.py
+++ utils/db.py
@@ -2,6 +2,17 @@
 import MySQLdb as mdb
 import helper as helper
 
+def getGroupID(group):
+	'''Get the binaryID of a group from mysql'''
+	mysqlInfo = helper.getMySQLInfo()
+	conn = mdb.connect(*mysqlInfo)
+	c = conn.cursor()
+	c.execute('SELECT ID FROM groups WHERE name = %s LIMIT 1', (group,))
+	resp = c.fetchone()
+	conn.close()
+	return int(resp[0])
+
+
 def getLastArticle(group):
 	'''Get the latest article for a group in the db. Arguments:
 	- sqldb: the sqlite3 file (str)
@@ -11,8 +22,8 @@ def getLastArticle(group):
 	mysqlInfo = helper.getMySQLInfo()
 	conn = mdb.connect(*mysqlInfo)
 	c = conn.cursor()
-	sth = c.execute('SELECT number FROM parts WHERE binaryID = %s ORDER BY number DESC LIMIT 1', (group,))
-	resp = sth.fetchone()
+	c.execute('SELECT last_record FROM parts WHERE binaryID = %s ORDER BY last_record DESC LIMIT 1', (group,))
+	resp = c.fetchone()
 	conn.close()
 	try:
 		return int(resp[0])
