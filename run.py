be9840ff91cc67a3b4a2ac212a6bc52934775252
convict <convict@jailcell.org>
2013-04-15 15:00:31 -0400

    Part insertion working

--- run.py
+++ run.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python
 import utils.db as db
 import utils.helper as helper
-import config
-import sqlite3 # just for the excpetions atm
+from pprint import pprint
 
-# connect to the server
-NNTP_INFO = [config.NNTP_Address, config.NNTP_Port, config.NNTP_SSL, config.NNTP_Username, config.NNTP_Password]
+NNTP_INFO = helper.getUsenetInfo()
 socket = helper.con(*NNTP_INFO)
 
 # get the welcome msg
 print socket.getwelcome()
 
-# show group overview
-print helper.group_overview(socket, 'alt.binaries.x264')
+_, _, first, last, _ = socket.group('alt.binaries.x264')
+# update groups table here
+gid = db.getGroupID('alt.binaries.x264')
+lastDBArticle = db.getLastArticle('alt.binaries.x264')
 
-# update group (only does headers for now)
-try:
-	resp = helper.update_group(socket, config.SQLDB, 'alt.binaries.x264')
-	print resp
-except sqlite3.OperationalError, e:
-	# create the parts table if it doesn't exist and re-run update_group
-	if 'table: parts' in str(e):
-		conn = sqlite3.connect(config.SQLDB)
-		c = conn.cursor()
-		c.execute('CREATE TABLE parts (group_name TEXT, article_id int, message_id TEXT, subject TEXT, date_posted TEXT, dt datetime default current_timestamp)')
-		conn.commit()
-		conn.close()
-		resp = helper.update_group(socket, config.SQLDB, 'alt.binaries.x264')
-		print resp
-	else:
-		print e
-		
-	
+if lastDBArticle == 0:
+	lastDBArticle = last - 100
+
+resp, overviews = socket.over((lastDBArticle, last))
+db.addParts(gid, overviews)
 
 # CLOSE THE SOCKET FOR THE LOVE OF GOD CLOSE THE FUCKING SOCKET
 socket.quit()
--- utils/db.py
+++ utils/db.py
@@ -2,6 +2,17 @@
 import MySQLdb as mdb
 import helper as helper
 
+def addParts(binaryID, over):
+	mysqlInfo = helper.getMySQLInfo()
+	conn = mdb.connect(*mysqlInfo)
+	c = conn.cursor()
+	sql = 'INSERT INTO parts (binaryID, messageID, number, partnumber, size, dateadded) VALUES (%s, %s, %s, %s, %s, NOW())'
+	params = [(binaryID, i[1]['message-id'], i[0], '1', i[1][':bytes']) for i in over]
+	c.executemany(sql, params)
+	conn.commit()
+	conn.close()
+	
+
 def getGroupID(group):
 	'''Get the binaryID of a group from mysql'''
 	mysqlInfo = helper.getMySQLInfo()
@@ -22,7 +33,7 @@ def getLastArticle(group):
 	mysqlInfo = helper.getMySQLInfo()
 	conn = mdb.connect(*mysqlInfo)
 	c = conn.cursor()
-	c.execute('SELECT last_record FROM parts WHERE binaryID = %s ORDER BY last_record DESC LIMIT 1', (group,))
+	c.execute('SELECT last_record FROM groups WHERE ID = %s ORDER BY last_record DESC LIMIT 1', (group,))
 	resp = c.fetchone()
 	conn.close()
 	try:
