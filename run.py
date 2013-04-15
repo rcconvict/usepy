6b0f63df0c93bfd5097ae088b822296f92271f50
convict <convict@jailcell.org>
2013-04-15 15:20:05 -0400

    Added some comments.

--- run.py
+++ run.py
@@ -9,21 +9,28 @@ socket = helper.con(*NNTP_INFO)
 # get the welcome msg
 print socket.getwelcome()
 
+# change group and get info
 _, _, first, last, _ = socket.group('alt.binaries.x264')
-# update groups table here
+
+# get groupID and last local article
 gid = db.getGroupID('alt.binaries.x264')
 lastDBArticle = db.getLastArticle(gid)
 
+# get last 100 articles on server if we don't ahve any
 if lastDBArticle == 0:
 	lastDBArticle = last - 100
 
+# get article headers and add them to the DB
 resp, overviews = socket.over((lastDBArticle, last))
 db.addParts(gid, overviews)
+
+# update the group with new info
 db.updateGroup(gid, last)
 
 # CLOSE THE SOCKET FOR THE LOVE OF GOD CLOSE THE FUCKING SOCKET
 socket.quit()
 
+# stats
 print 'Last local article num: %s' % lastDBArticle
 print 'Last server article num: %s' % last
 print 'Added %d parts.' % (last-lastDBArticle)
