a81f27ec03746b8b2b7b030b069941fd7a49c45f
convict <convict@jailcell.org>
2013-05-08 09:15:43 -0400

    Fixed error when translate is fed a unicode string.

--- utils/namecleaning.py
+++ utils/namecleaning.py
@@ -2,6 +2,7 @@
 import re
 
 def collectionsCleaner(subject):
+	subject = subject.encode('ascii', 'ignore')
 	# parts/files
 	cleanSubject = re.sub('(\(|\[|\s)\d{1,4}(\/|(\s|_)of(\s|_)|\-)\d{1,4}(\)|\]|\s)|\(\d{1,3}\|\d{1,3}\)|\-\d{1,3}\-\d{1,3}\.|\s\d{1,3}\sof\s\d{1,3}\.|\s\d{1,3}\/\d{1,3}|\d{1,3}of\d{1,3}\./i', '', subject)
 	# Anything between the quotes. Too much variance within the quotes, so remove it completely
@@ -16,6 +17,7 @@ def collectionsCleaner(subject):
 	return cleanSubject
 
 def releaseCleaner(subject):
+	subject = subject.encode('ascii', 'ignore')
 	# file and part count
 	cleanerName = re.sub('(\(|\[|\s)\d{1,4}(\/|(\s|_)of(\s|_)|\-)\d{1,4}(\)|\]|\s)|\(\d{1,3}\|\d{1,3}\)|\-\d{1,3}\-\d{1,3}\.|\s\d{1,3}\sof\s\d{1,3}\.|\s\d{1,3}\/\d{1,3}|\d{1,3}of\d{1,3}\./i', '', subject)
 	# size
@@ -37,6 +39,7 @@ def releaseCleaner(subject):
 	return cleanerName
 
 def fixerCleaner(name):
+	subject = subject.encode('ascii', 'ignore')
 	# extensions
 	cleanerName = re.sub('(\.part(\d{1,5})?)?\.(7z|\d{3}(?=(\s|"))|avi|epub|exe|idx|jpg|mobi|mp4|nfo|nzb|par\s?2|pdf|rar|rev|r\d\d|sfv|srs|srr|sub|txt|vol.+(par2)|zip|z{2})"?|\d{2,3}\.pdf|yEnc|\.part\d{1,4}\./i', '', name)
 	# remove some chars	
