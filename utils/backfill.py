9a954e2431f38ad0fceb2b536352e71d788260a5
convict <convict@jailcell.org>
2013-05-10 12:05:14 -0400

    Init port for backfill.

---
+++ utils/backfill.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from dateutil.parser import parse
+
+class Backfill():
+	def __init__(self):
+		self.n = "\n"
+
+	def postdate(self, nntp, post, debug=True):
+		n = self.n
+		attempts = 0
+		success = False
+
+		while attempts <= 3 and success == False:
+			resp, msgs = nntp.over((post, post))
+			try:
+				date = msgs[0][1]['date']
+				date = parse(date).strftime('%s')
+				success = True
+			except:
+				attempts += 1
+
+		if debug: print 'DEBUG: postdate for post: %s came back %s' % (post, date)
+
+		try:
+			return date
+		except UnboundLocalError:
+			return ''
